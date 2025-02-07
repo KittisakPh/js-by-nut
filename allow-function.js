f3c5f44b227f447b0cc45562bc1a99f0b52760d8
Kittisak2001 <kittisak.ph2001@gmail.com>
2025-02-08 02:21:40 +0700

    feat(arrow): basic arrow function

---
+++ allow-function.js
@@ -0,0 +1,19 @@
+const showMessage = () => {
+  console.log('Hello, world!!!')
+}
+console.log(showMessage())
+
+let sum = (a, b) => a + b
+console.log(sum(5, 5))
+
+let double = (n) => n * n
+console.log(double(5))
+
+let sayHi = () => console.log('Hello World!')
+sayHi()
+
+let add = (a, b) => {
+  const result = a + b
+  return result;
+}
+console.log(add(5, 5))
